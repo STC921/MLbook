b24554c10fd6b2ef42567a39f448a3f93a6a0d3d
STC921 <you@example.com>
2024-10-17 12:06:11 +0900

    initial commit

--- PyTorch-Lighting.py
+++ PyTorch-Lighting.py
@@ -1,6 +1,9 @@
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+from torchvision import transforms
 
 from torchmetrics import Accuracy
 
@@ -8,12 +11,12 @@ class MultiLayerPerceptron(pl.LightningModule):
     def __init__(self, image_shape=(1, 28, 28), hidden_units=(32, 16)):
         super().__init__()
 
-        self.train_acc = Accuracy()
-        self.valid_acc = Accuracy()
-        self.test_acc = Accuracy()
+        self.train_acc = Accuracy(task="multiclass", num_classes=10)
+        self.valid_acc = Accuracy(task="multiclass", num_classes=10)
+        self.test_acc = Accuracy(task="multiclass", num_classes=10)
 
         input_size = image_shape[0] * image_shape[1] * image_shape[2]
-        all_layers = []
+        all_layers = [nn.Flatten()]
         for hidden_unit in hidden_units:
             all_layers.append(nn.Linear(input_size, hidden_unit))
             all_layers.append(nn.ReLU())
@@ -34,8 +37,11 @@ class MultiLayerPerceptron(pl.LightningModule):
         self.log("train_loss", loss, prog_bar=True)
         return loss
 
-    def training_epoch_end(self, outs):
+    def on_train_epoch_end(self):
+        # 记录训练准确率
         self.log("train_acc", self.train_acc.compute())
+        # 重置准确率计算器，以便下一 epoch 使用
+        self.train_acc.reset()
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
@@ -59,3 +65,50 @@ class MultiLayerPerceptron(pl.LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=0.001)
+
+class MnistDataModule(pl.LightningDataModule):
+    def __init__(self, data_path='./'):
+        super().__init__()
+        self.data_path = data_path
+        self.transform = transforms.Compose([transforms.ToTensor()])
+
+    def prepare_data(self):
+        MNIST(root=self.data_path, download=True)
+
+    def setup(self, stage=None):
+        mnist_all = MNIST(
+            root=self.data_path,
+            train=True,
+            transform=self.transform,
+            download=False,
+        )
+        self.train, self.val = random_split(
+            mnist_all, [55000, 5000], generator=torch.Generator().manual_seed(1)
+        )
+        self.test = MNIST(
+            root=self.data_path,
+            train=False,
+            transform=self.transform,
+            download=False,
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=64, num_workers=4)
+
+    def val_dataloader(self):
+        return DataLoader(self.val, batch_size=64, num_workers=4)
+
+    def test_dataloader(self):
+        return DataLoader(self.test, batch_size=64, num_workers=4)
+
+if __name__ == '__main__':
+    torch.manual_seed(1)
+    mnist_dm = MnistDataModule()
+
+    mnistclassifier = MultiLayerPerceptron()
+    if torch.cuda.is_available():
+        trainer = pl.Trainer(max_epochs=10, accelerator="gpu", devices=1)
+    else:
+        trainer = pl.Trainer(max_epochs=10)
+
+    trainer.fit(model=mnistclassifier, datamodule=mnist_dm)
