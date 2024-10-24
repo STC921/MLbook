05abb3f9e63009d5e2b99ad3e4c913445fe673c3
STC921 <you@example.com>
2024-10-24 15:03:57 +0900

    initial commit

---
+++ 17 Generative Adversarial Networks  for Synthesizing New Data.py
@@ -0,0 +1,191 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+def make_generator_network(
+        input_size=20,
+        num_hidden_layers=1,
+        num_hidden_units=100,
+        num_output_units=784):
+    model = nn.Sequential()
+    for i in range(num_hidden_layers):
+        model.add_module(f'fc_g{i}', nn.Linear(input_size, num_hidden_units))
+        model.add_module(f'relu_g{i}', nn.LeakyReLU())
+        input_size = num_hidden_units
+    model.add_module(f'fc_g{num_hidden_layers}', nn.Linear(input_size, num_output_units))
+    model.add_module('tanh_g', nn.Tanh())
+    return model
+
+def make_discriminator_network(
+        input_size,
+        num_hidden_layers=1,
+        num_hidden_units=100,
+        num_output_units=1):
+    model = nn.Sequential()
+    for i in range(num_hidden_layers):
+        model.add_module(f'fc_d{i}', nn.Linear(input_size, num_hidden_units, bias=False))
+        model.add_module(f'relu_d{i}', nn.LeakyReLU())
+        model.add_module('dropout', nn.Dropout(0.5))
+        input_size = num_hidden_units
+    model.add_module(f'fc_d{num_hidden_layers}', nn.Linear(input_size, num_output_units))
+    model.add_module('sigmoid', nn.Sigmoid())
+    return model
+
+image_size = (28, 28)
+z_size = 20
+gen_hidden_layers = 1
+gen_hidden_size = 100
+disc_hidden_layers = 1
+disc_hidden_size = 100
+torch.manual_seed(1)
+gen_model = make_generator_network(
+    input_size=z_size,
+    num_hidden_layers=gen_hidden_layers,
+    num_hidden_units=gen_hidden_size,
+    num_output_units=np.prod(image_size)
+)
+disc_model = make_discriminator_network(
+    input_size=np.prod(image_size),
+    num_hidden_layers=disc_hidden_layers,
+    num_hidden_units=disc_hidden_size
+)
+print(gen_model); print(disc_model)
+
+import torchvision
+from torchvision import transforms
+image_path = './'
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5), std=(0.5))
+])
+mnist_dataset = torchvision.datasets.MNIST(
+    root=image_path, train=True,
+    transform=transform, download=False
+)
+example, label = next(iter(mnist_dataset))
+print(f'Min:{example.min()} Max:{example.max()}')
+print(example.shape)
+
+def create_noise(batch_size, z_size, mode_z):
+    if mode_z == 'uniform':
+        input_z = torch.rand(batch_size, z_size)*2 - 1
+    elif mode_z == 'normal':
+        input_z = torch.randn(batch_size, z_size)
+    return input_z
+
+from torch.utils.data import DataLoader
+# batch_size = 32
+# dataloader = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=False)
+# input_real, label = next(iter(dataloader))
+# input_real = input_real.view(batch_size, -1)
+# torch.manual_seed(1)
+# mode_z = 'uniform'
+# input_z = create_noise(batch_size, z_size, mode_z)
+# print('input-z -- shape:', input_z.shape)
+# print('input-real -- shape:', input_real.shape)
+#
+# g_output = gen_model(input_z)
+# d_proba_real = disc_model(input_real)
+# d_proba_fake = disc_model(g_output)
+# print('Output of G -- shape:', g_output.shape)
+# print('Disc. (real) -- shape:', d_proba_real.shape)
+# print('Disc. (fake) -- shape:', d_proba_fake.shape)
+#
+# loss_fn = nn.BCELoss()
+# g_labels_real = torch.ones_like(d_proba_fake)
+# g_loss = loss_fn(d_proba_fake, g_labels_real)
+# print(f'Generator Loss: {g_loss:.4f}')
+#
+# d_labels_real = torch.ones_like(d_proba_real)
+# d_labels_fake = torch.zeros_like(d_proba_fake)
+# d_loss_real = loss_fn(d_proba_real, d_labels_real)
+# d_loss_fake = loss_fn(d_proba_fake, d_labels_fake)
+# print(f'Discriminator Lossed: Real {d_loss_real:.4f} Fake {d_loss_fake:.4f}')
+
+batch_size = 64
+torch.manual_seed(1)
+np.random.seed(1)
+mnist_dl = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+gen_model = make_generator_network(
+    input_size=z_size,
+    num_hidden_layers=gen_hidden_layers,
+    num_hidden_units=gen_hidden_size,
+    num_output_units=np.prod(image_size)
+).to(device)
+disc_model = make_discriminator_network(
+    input_size=np.prod(image_size),
+    num_hidden_layers=disc_hidden_layers,
+    num_hidden_units=disc_hidden_size,
+).to(device)
+
+loss_fn = nn.BCELoss()
+g_optimizer = torch.optim.Adam(gen_model.parameters())
+d_optimizer = torch.optim.Adam(disc_model.parameters())
+
+mode_z = 'uniform'
+
+def d_train(x):
+    disc_model.zero_grad()
+    batch_size = x.size(0)
+    x = x.view(batch_size, -1).to(device)
+    d_labels_real = torch.ones(batch_size, 1, device=device)
+    d_proba_real = disc_model(x)
+    d_loss_real = loss_fn(d_proba_real, d_labels_real)
+    input_z = create_noise(batch_size, z_size, mode_z).to(device)
+    g_output = gen_model(input_z)
+    d_proba_fake = disc_model(g_output)
+    d_labels_fake = torch.zeros(batch_size, 1, device=device)
+    d_loss_fake = loss_fn(d_proba_fake, d_labels_fake)
+    d_loss = d_loss_real + d_loss_fake
+    d_loss.backward()
+    d_optimizer.step()
+    return d_loss.data.item(), d_proba_real.detach(), d_proba_fake.detach()
+
+def g_train(x):
+    gen_model.zero_grad()
+    batch_size = x.size(0)
+    input_z = create_noise(batch_size, z_size, mode_z).to(device)
+    g_labels_real = torch.ones(batch_size, 1, device=device)
+    g_output = gen_model(input_z)
+    d_proba_fake = disc_model(g_output)
+    g_loss = loss_fn(d_proba_fake, g_labels_real)
+    g_loss.backward()
+    g_optimizer.step()
+    return g_loss.data.item()
+
+fixed_z = create_noise(batch_size, z_size, mode_z).to(device)
+def create_samples(g_model, input_z):
+    g_output = g_model(input_z)
+    images = torch.reshape(g_output, (batch_size, *image_size))
+    return (images+1)/2.0
+
+epoch_samples = []
+all_d_losses = []
+all_g_losses = []
+all_d_real = []
+all_d_fake = []
+num_epochs = 100
+
+for epoch in range(1, num_epochs+1):
+    d_losses, g_losses = [], []
+    d_vals_real, d_vals_fake = [], []
+    for i, (x, _) in enumerate(mnist_dl):
+        d_loss, d_proba_real, d_proba_fake = d_train(x)
+        d_losses.append(d_loss)
+        g_losses.append(g_train(x))
+        d_vals_real.append(d_proba_real.mean().cpu())
+        d_vals_fake.append(d_proba_fake.mean().cpu())
+
+    all_d_losses.append(torch.tensor(d_losses).mean())
+    all_g_losses.append(torch.tensor(g_losses).mean())
+    all_d_real.append(torch.tensor(d_vals_real).mean())
+    all_d_fake.append(torch.tensor(d_vals_fake).mean())
+    print(f'Epoch {epoch:03d} | Avg Losses >> G/D {all_g_losses[-1]:.4f}/{all_d_losses[-1]:.4f} [D_Real:{all_d_real[-1]:.4f} D-Fake:{all_d_fake[-1]:.4f}]')
+    epoch_samples.append(create_samples(gen_model, fixed_z).detach().cpu().numpy())
